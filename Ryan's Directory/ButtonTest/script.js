5261fbe1ab717a61a52e479af3df6c38644934ac
Radams5000 <ryan.adams.5000@gmail.com>
2021-02-11 19:22:35 -0700

    Add files via upload

---
+++ Ryan's Directory/ButtonTest/script.js
@@ -0,0 +1,13 @@
+var quotes =['The only true knowledge is knowing we know nothing','All we are is dust in the wind dude', 'Dust, wind, dude','Like sands in an hour glass, so are the days of our lives','Adams Rulz']
+
+function newQuote(){
+  var randomNumber = Math.floor(Math.random()*(quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
+
+function guardarNumeros(){
+ boxvalue = document.getElementById('box').value;
+ quotes.push(boxvalue);  
+ console.log(quotes);
+ return false;
+}
