17ed5921c987939ff13296865868ace50fd7865b
Radams5000 <ryan.adams.5000@gmail.com>
2021-03-05 16:09:59 -0700

    Update taskpane.js

--- Mar2v1/src/taskpane/taskpane.js
+++ Mar2v1/src/taskpane/taskpane.js
@@ -10,34 +10,25 @@ import "../../assets/icon-80.png";
 
 /* global document, Office */
 
-Office.onReady(info => {
-  if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
-});
-
-function guardarNumeros() {
-    boxvalue = document.getElementById('box').value;
-    quotes.push(boxvalue);
-    console.log(quotes);
-    return false;
-}
 
 var quotes = ['The only true knowledge is knowing we know nothing', 'All we are is dust in the wind dude', 'Dust, wind, dude', 'Like sands in an hour glass, so are the days of our lives', 'Adams Rulz']
 
+
+Office.onReady(info => {
+  if (info.host === Office.HostType.Outlook) {
+
+      document.getElementById("run").onclick = run;
+
+  }
+});
+
+
+
 export async function run() {
-
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
-
-
-    // Check if the add-in has been configured.
-    Office.context.mailbox.item.body.setSelectedDataAsync("testTwo",
-        { coercionType: Office.CoercionType.Html }, function (result) {
-            event.completed();
-        });
   /**
    * Insert your Outlook code here
    */
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+    Office.context.mailbox.item.body.setSelectedDataAsync(quotes[randomNumber])
 }
