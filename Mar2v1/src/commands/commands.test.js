d822058bdeff699e86cc89ca75a292e4ae5af8ca
Patrick <pmdino93@gmail.com>
2021-03-28 12:12:46 -0600

    Update commands.test.js

--- Mar2v1/src/commands/commands.test.js
+++ Mar2v1/src/commands/commands.test.js
@@ -1,5 +1,5 @@
 const insertDefaultGist = require('./insertDefaultGist');
 
 Test('Test that the random number gives a value within the expected range', () => {
-  expect()  
+  expect(insertDefaultGist.Office.context.mailbox.item.body.setSelectedDataAsync(quotes[1]).toBe('All we are is dust in the wind dude'))  
 });
