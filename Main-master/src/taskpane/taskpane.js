f54bada6a3410888bda3c57529c650fb27c31877
Jaden Cua <jcua@msudenver.edu>
2021-04-21 08:49:45 -0600

    Minor tweaks

--- Main-master/src/taskpane/taskpane.js
+++ Main-master/src/taskpane/taskpane.js
@@ -1,14 +1,11 @@
 /*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
+
  */
 
-// images references in the manifest
+// Images references are in the manifest
 import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
-
-/* global document, Office */
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
@@ -22,7 +19,9 @@ export async function run() {
    */
 }
 
-/*const form = document.querySelector('form')
+/*Prior:
+ 
+const form = document.querySelector('form')
 const ul = document.querySelector('ul')
 const button = document.querySelector('button')
 const input = document.getElementById('item')
