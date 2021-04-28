44458a87ebeb4b6b8e6b36838c05a1480c38c862
Radams5000 <ryan.adams.5000@gmail.com>
2021-04-28 07:29:38 -0600

    I added a "personal" button to show quotes for different emails.

--- Main-master/src/taskpane/taskpane.js
+++ Main-master/src/taskpane/taskpane.js
@@ -16,7 +16,8 @@ Office.onReady(info => {
     document.getElementById("applyRandom").onclick = randomSig;
     document.getElementById("applySelected").onclick = applySig;
     document.getElementById("addSig").onclick = addQuote;
-    document.getElementById("loadQuotes").onclick = getQuotes;
+      document.getElementById("loadQuotes").onclick = getQuotes;
+      document.getElementById("loadStarQuotes").onclick = getStarQuotes;
   }
 });
 
@@ -33,6 +34,26 @@ var quotes = [' If you don\'t know where you\'re going, any road will get you th
     ' We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology. -Carl Sagan ',
     ' The characteristic of scientific progress is our knowing what we did not know. -Gaston Bachelard ', ' Comedy is simply a funny way of being serious. -Peter Ustinov ',
     ' A sense of humor... is needed armor. Joy in one\'s heart and some laughter on one\'s lips is a sign that the person down deep has a pretty good grasp of life. -Hugh Sidey ']
+
+var starQuotes = [' No, I am your father - Darth Vader',
+    ' I got a bad feeling about this - Han Solo',
+    'General Kenobi - General Grievous',
+    ' I find your lack of faith disturbing - Darth Vader',
+    ' Do or do not there is no try - Yoda',
+    ' Hello there - Obi wan Kenobi',
+    ' This is where the fun begins - Anakin',
+    ' When gone am I the last jedi you will be - Yoda',
+    ' 100,000 troops are ready with a million more well on the way - IDK',
+    ' I have the high ground - Obi wan',
+    ' I don\'t like sand. It\'s rough, course, irritating, and it gets everywhere - Anakin',
+    ' Use the force, Luke - Obi Wan', ' Your focus determines your reality. -Qui Gin',
+    ' Perhaps the archives are incomplete - Obi Wan',
+    ' I Suggest A New Strategy, R2 - Let The Wookiee Win  - C3PO',
+    ' Good luck -Ahsoka Tano',
+    ' You where right about one thing master. The negotiations where short. - Obi wan',
+    ' I am the Senate - Sheev Palpatine',
+    ' What about the droid attack on the wookies? - Ki - Adi Mundi',
+    ' Kenobiiiiiiiiiiah!!! - Darthmaul']
 var option = '';
 
 function randomSig() {
@@ -57,9 +78,20 @@ function addQuote() {
 }
 
 function getQuotes() {
+    option = '';
     for (let i = 0; i < quotes.length; i++) {
         option += '<option value="' + quotes[i] + '">'
         console.log(quotes[i]);
+    }
+    document.getElementById("signatures").innerHTML = option;
+
+}
+
+function getStarQuotes() {
+    option = '';
+    for (let i = 0; i < starQuotes.length; i++) {
+        option += '<option value="' + starQuotes[i] + '">'
+        console.log(starQuotes[i]);
     }
     document.getElementById("signatures").innerHTML = option;
 
