50922f711aeaeab1489b7c9de6d59c658a4c8e10
Patrick <pmdino93@gmail.com>
2021-05-08 10:30:42 -0600

    random test (still no passes)

--- Main-Master/src/taskpane/taskpane.test.js
+++ Main-Master/src/taskpane/taskpane.test.js
@@ -5,6 +5,15 @@ var addQuote = import("addQuote");
 const taskpane = require("./taskpane");
 const quotes = taskpane.quotes;
 const starQuotes = taskpane.starQuotes;
-document.getElementById('newSig').value = "test";
-taskpane.addQuote();
-expect(quotes[quotes.length - 1]).toEqual("test");
+
+test('Add a signature to the signature list' , () => {
+    document.getElementById('newSig').value = "test";
+    taskpane.addQuote();
+    expect(quotes[quotes.length - 1]).toEqual("test");
+})
+
+test('Test random number is giving valid values' , () => {
+    const randomNum = Math.floor(Math.random() * (quotes.length));
+    expect(randomNum).not.toEqual(null);
+    expect(randomNum).toBeLessThanOrEqual(quotes.length);
+})
